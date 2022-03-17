d99139829a9055ea9a89f6ae3e5d8284b6b9423e
lockchr <chrislock001@gmail.com>
2022-03-16 21:10:38 -0600

    created observer for auth in centralized context, to be used in place of hooks and prop drilling. Cleaned up code a little

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-const rootElement = document.getElementById('root')
+import "./index.scss";
+import { UserProvider } from "./contexts/user.context";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   rootElement
 );
-
